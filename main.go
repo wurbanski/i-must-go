1bc9eb96b61337ca1eed8ad87473e5a67c12ff21
urban <urban@ds.pg.gda.pl>
2017-03-16 01:06:03 +0100

    Removed unnecessary code, switched OID, created first step of crawling

--- main.go
+++ main.go
@@ -1,47 +1,69 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"log"
-	"math/big"
 	"strconv"
 	str "strings"
+	"time"
 
 	gosnmp "github.com/soniah/gosnmp"
 )
 
-const (
-	ChassisIdSubtype = 4 + iota // 4
-	ChassisId                   // 5
-	PortIdSubtype               // 6
-	PortId                      // 7
-	PortDesc                    // 8
-	SysName                     // 9
-	SysDesc                     // 10
-)
-
 type Port struct {
-	ChassisIdSubtype *big.Int // 4
-	ChassisId        string   // 5
-	PortIdSubtype    *big.Int // 6
-	PortId           *big.Int // 7
-	PortDesc         string   // 8
-	SysName          string   // 9
-	SysDesc          string   // 10
+	RemoteSwitch *networkSwitch
+}
+
+type networkSwitch struct {
+	IP    string
+	Ports map[int]*Port
+}
+
+func NewNetworkSwitch(IP string) *networkSwitch {
+	return &networkSwitch{
+		IP:    IP,
+		Ports: make(map[int]*Port),
+	}
+}
+
+func (n networkSwitch) FindRemotes() {
+	snmpConnection := &gosnmp.GoSNMP{
+		Target:    n.IP,
+		Port:      161,
+		Community: "DS-public",
+		Version:   gosnmp.Version2c,
+		Timeout:   time.Duration(2) * time.Second,
+	}
+
+	err := snmpConnection.Connect()
+	if err != nil {
+		log.Fatalf("Connect() err: %v", err)
+	}
+	defer snmpConnection.Conn.Close()
+
+	lldpOid := "1.0.8802.1.1.2.1.4.2.1"
+
+	err2 := snmpConnection.BulkWalk(lldpOid, n.interpretValue)
+	if err2 != nil {
+		log.Fatalf("Get() err: %v", err2)
+	}
+
+}
+
+func (n networkSwitch) String() string {
+	return fmt.Sprintf("Switch: %s\nPorts: %s", n.IP, n.Ports)
+}
+
+func (n networkSwitch) ShowPorts() string {
+	return fmt.Sprintf("%s", n.Ports)
 }
 
 func (p Port) String() string {
-	return fmt.Sprintf("Remote:\n Chassis: %s\n Port: %d (%s)\n System: %s\n", p.ChassisId, p.PortId, p.PortDesc, p.SysName)
+	return fmt.Sprintf("Remote IP:%s\n", p.RemoteSwitch.IP)
 }
 
-func unpackPduName(s string) (int, int) {
+func unpackPduName(s string) (int, string) {
 	splitOid := str.Split(s, ".")
-	functId, err := strconv.Atoi(splitOid[11])
-	if err != nil {
-		// handle error
-		fmt.Println(err)
-	}
 
 	portId, err := strconv.Atoi(splitOid[13])
 	if err != nil {
@@ -49,58 +71,41 @@ func unpackPduName(s string) (int, int) {
 		fmt.Println(err)
 	}
 
-	return functId, portId
+	remoteIP := str.Join(splitOid[17:], ".")
+
+	return portId, remoteIP
 }
 
-func interpretValue(pdu gosnmp.SnmpPDU) error {
+func (n *networkSwitch) interpretValue(pdu gosnmp.SnmpPDU) error {
 
-	funcId, portId := unpackPduName(pdu.Name)
+	portId, remoteIP := unpackPduName(pdu.Name)
 
-	_, ok := ports[portId]
+	nswitch, ok := switches[remoteIP]
 	if !ok {
-		ports[portId] = &Port{}
+		nswitch = NewNetworkSwitch(remoteIP)
+		switches[remoteIP] = nswitch
 	}
 
-	switch funcId {
-	case ChassisIdSubtype:
-		ports[portId].ChassisIdSubtype = gosnmp.ToBigInt(pdu.Value)
-	case ChassisId:
-		hexEncoded := hex.EncodeToString(pdu.Value.([]byte))
-		ports[portId].ChassisId = hexEncoded
-	case PortIdSubtype:
-		ports[portId].PortIdSubtype = gosnmp.ToBigInt(pdu.Value)
-	case PortId:
-		ports[portId].PortId = gosnmp.ToBigInt(pdu.Value)
-	case PortDesc:
-		ports[portId].PortDesc = string(pdu.Value.([]byte))
-	case SysName:
-		ports[portId].SysName = string(pdu.Value.([]byte))
-	case SysDesc:
-		ports[portId].SysDesc = string(pdu.Value.([]byte))
+	_, ok = n.Ports[portId]
+	if !ok {
+		n.Ports[portId] = &Port{nswitch}
 	}
 
 	return nil
 }
 
 var ports = make(map[int]*Port)
+var switches = make(map[string]*networkSwitch)
 
 func main() {
-	gosnmp.Default.Target = "10.1.1.1"
-	gosnmp.Default.Community = "DS-public"
-	err := gosnmp.Default.Connect()
-	if err != nil {
-		log.Fatalf("Connect() err: %v", err)
-	}
-	defer gosnmp.Default.Conn.Close()
 
-	lldpOid := "1.0.8802.1.1.2.1.4.1.1"
+	startIP := "10.1.1.0"
+	startSwitch := NewNetworkSwitch(startIP)
+	switches[startIP] = startSwitch
 
-	err2 := gosnmp.Default.BulkWalk(lldpOid, interpretValue)
-	if err2 != nil {
-		log.Fatalf("Get() err: %v", err2)
-	}
+	startSwitch.FindRemotes()
 
-	for index, port := range ports {
-		fmt.Printf("Port %d:\n%s", index, port)
+	for index, nswitch := range switches {
+		fmt.Printf("* Switch %s: %s\n", index, nswitch.ShowPorts())
 	}
 }
